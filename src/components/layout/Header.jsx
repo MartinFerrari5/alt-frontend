8feee5b6fbc0ea29744182c3d95f6386067344b9
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2025-04-02 14:22:17 -0300

    fix: estilos de botones

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,3 @@
-// src/components/layout/Header.jsx
 import PropTypes from "prop-types"
 import { useState } from "react"
 import { useLocation, useSearchParams } from "react-router-dom"
@@ -32,33 +31,42 @@ function Header({ subtitle, title, tasks }) {
     const showDownloadExcel = currentPath === "/" || currentPath === "/history"
 
     return (
-        <div className="flex w-full justify-between">
+        <div className="flex w-full justify-between items-center py-4 px-6 bg-white shadow-md rounded-lg">
             <div>
                 <span className="text-xs font-semibold text-brand-custom-green">
                     {subtitle}
                 </span>
-                <h2 className="text-xl font-semibold">{title}</h2>
+                <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
             </div>
 
             <div className="flex items-center gap-3">
+                <div>
+
+                    {isAdminPath ? (
+                        <>
+                            <AddOptionDialog
+                                isOpen={addDialogIsOpen}
+                                handleClose={() => setAddDialogIsOpen(false)}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <AddTaskDialog
+                                isOpen={addDialogIsOpen}
+                                handleClose={() => setAddDialogIsOpen(false)}
+                            />
+                        </>
+                    )}
+                </div>
                 {showDownloadExcel && <DownloadExcelButton tasks={tasks} />}
 
-                <Button onClick={() => setAddDialogIsOpen(true)}>
+                <Button
+                    onClick={() => setAddDialogIsOpen(true)}
+                >
                     <AddIcon />
                     {adminPath ? "" : "Nueva tarea"}
                 </Button>
 
-                {isAdminPath ? (
-                    <AddOptionDialog
-                        isOpen={addDialogIsOpen}
-                        handleClose={() => setAddDialogIsOpen(false)}
-                    />
-                ) : (
-                    <AddTaskDialog
-                        isOpen={addDialogIsOpen}
-                        handleClose={() => setAddDialogIsOpen(false)}
-                    />
-                )}
 
                 {currentPath === "/" && (
                     <SendToRRHHButton
